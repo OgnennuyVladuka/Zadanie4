91c2fb4f6522fc221fd660960f4c76bb9b40259f
OgnennuyVladuka <146809006+OgnennuyVladuka@users.noreply.github.com>
2023-10-08 22:26:01 +0300

    Add files via upload

---
+++ z4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace z4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до файла:");
+            string s = Console.ReadLine();
+            string[] names = s.Split('/');
+            int dlin = names.Length;
+            Console.WriteLine("Имя файла:\r\n" +
+                (names[dlin - 1]));
+        }
+    }
+}
